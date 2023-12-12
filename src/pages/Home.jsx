6c89db75e915ae35c59bfaca6f759f8e263af40e
Duwal <riyeshshrestha1094@gmail.com>
2023-12-12 22:18:03 +0545

    modified:   src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     <div className="bg-custom3">
     <div className='flex justify-center items-center  lg:pt-12 lg:flex-row flex-col '>
             <CustomCarousel/>
-            <div className="lg:w-[70%]  lg:text-6xl lg:p-8 text-md  p-3 font-medium  bg-black  lg:bg-custom3 text-white  lg:text-black">
+            <div className="lg:w-[70%]  lg:text-6xl lg:p-8 text-md  p-3 font-medium  bg-custom1  lg:bg-custom3 text-white  lg:text-black">
               <p className='text-center lg:font-custom '>Everyday Luxe range featuring premium bed linen for everyday use.</p>
             </div>
           </div>
